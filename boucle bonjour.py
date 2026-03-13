8072f9b9cab6a0119eb3f1a4592aa8512d726cc0
AliciaMor <alicia.bariauxmorante@gmail.com>
2026-03-13 15:16:41 +0100

    Add greeting loop in boucle bonjour.py

---
+++ boucle bonjour.py
@@ -0,0 +1,5 @@
+print("Bonjour et bienvenu dans ce programme qui vous dit bonjour.")
+reponse=input("Voulez vous continuer ?")
+while reponse not in ["o","n"]:
+    print("Bonjour")
+    print("Voulez vous continuer ?")
